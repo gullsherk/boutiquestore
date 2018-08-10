fb192d42ed59b08b92cdc673c7968663f665a065
Ashhad Sheikh <ashhadsheikh@hotmail.com>
2018-08-10 18:52:41 +0500

    Add rails migration and models for boutique store

---
+++ db/migrate/20180810104935_add_botique_store_models.rb
@@ -0,0 +1,38 @@
+class AddBotiqueStoreModels < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :phone
+      t.string :password
+      t.string :authentication_token
+      t.timestamps
+    end
+
+    create_table :items do |t|
+      t.string :name
+      t.string :type
+      t.string :size
+      t.integer :price
+      t.timestamps
+    end
+
+    create_table :order do |t|
+      t.integer :user_id, references: :users
+      t.date :date
+      t.string :comments
+      t.float :sub_total
+      t.float :tax
+      t.float :discount
+      t.timestamps
+    end
+
+    create_table :order_items do |t|
+      t.integer :order_id, references: :orders
+      t.integer :item_id, references: :items
+      t.integer :quantity
+      t.timestamps
+    end
+  end
+end
