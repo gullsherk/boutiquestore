de285dd83802b72506923ebfcd71c211659e125b
Ashhad Sheikh <ashhadsheikh@hotmail.com>
2018-08-14 20:18:22 +0500

    add dropdown instead of string

--- app/admin/items.rb
+++ app/admin/items.rb
@@ -2,4 +2,16 @@ ActiveAdmin.register Item do
 
   permit_params :name, :type, :size, :price, :stock, :image
 
+    form do |f|
+    f.inputs do
+      f.input :name
+      f.input :type, as: :select, collection: Item::TYPES, include_blank: false
+      f.input :size, as: :select, collection: Item::SIZES, include_blank: false
+      f.input :price
+      f.input :stock
+      f.input :image
+    end
+    f.actions
+  end
+
 end
