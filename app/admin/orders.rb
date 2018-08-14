057bd9af8a57f2ff64f5ff3f83c8d6a82675b4a7
Ashhad Sheikh <ashhadsheikh@hotmail.com>
2018-08-14 15:56:17 +0500

    add changes to items model and accept stock attribute as well

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -3,6 +3,8 @@ ActiveAdmin.register Order do
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
 # permit_params :list, :of, :attributes, :on, :model
+
+actions :index, :show
 #
 # or
 #
@@ -11,5 +13,30 @@ ActiveAdmin.register Order do
 #   permitted << :other if params[:action] == 'create' && current_user.admin?
 #   permitted
 # end
+
+  show do
+    attributes_table do
+      row :id
+      row :user_id   
+      row :date    
+      row :comments  
+      row :sub_total 
+      row :tax       
+      row :discount
+      row :created_at
+      row :updated_at
+    end
+
+    panel 'Order Items', only: :show do
+      paginated_collection(order.order_items.order('created_at DESC').page(params[:page]).per(10), download_links: false) do
+        table_for collection do
+          column :id
+          column :order
+          column :item
+          column :quantity
+        end
+      end
+    end
+  end
 
 end
